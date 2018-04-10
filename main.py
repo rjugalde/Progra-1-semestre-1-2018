5d77659c0ee64fc1191f04ee4ed7c425881d90b3
rjugalde <ugalde.rj@gmail.com>
2018-04-10 05:48:50 -0600

    avance 3

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ LProfesores=[]
 import sys
 
 #Abrir archivos
-OpenCarreras = open("carreras.txt",'r')
-OpenCursos = open("cursos.txt",'r')
-OpenGrupos = open("grupos.txt",'r')
-OpenEstuCurso = open("estudiante-curso.txt",'r')
-Openestudiantes = open("estudiantes.txt",'r')
-OpenProfesores = open("profesores.txt",'r')
+OpenCarreras = open("Archivos/carreras.txt",'r')
+OpenCursos = open("Archivos/cursos.txt",'r')
+OpenGrupos = open("Archivos/grupos.txt",'r')
+OpenEstuCurso = open("Archivos/estudiante-curso.txt",'r')
+Openestudiantes = open("Archivos/estudiantes.txt",'r')
+OpenProfesores = open("Archivos/profesores.txt",'r')
 
-Reportes = open("REPORTE.TXT",'w')
+#Reportes = open("REPORTE.TXT",'a')
 
 def loadcarreras():
   for line in OpenCarreras:
@@ -57,99 +57,160 @@ def loadprofesores():
 
   ##QUITAR ULTIMO CARACTER x[:len(x)-1] ]
 ########## loops de reportes   #################
-def reportCarreras():
+def reportCarreras(Reportes):
+  Reportes.write("codCarrera,Nombre\n")
   for strings in LCarreras:
       cod_carrera=strings[0]
       nombre=strings[1]
-      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
-                "Nombre: "+nombre+"\n") 
-      Reportes.write("Carreras") 
+##      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
+##                "Nombre: "+nombre+"\n")
+      reporte=cod_carrera+","+nombre
+##      Reportes.write("Carreras") 
       Reportes.write(reporte)
-def reportCursos():
+def reportCursos(Reportes):
+  Reportes.write("CodCarrera,CodCurso,Nombre\n")
   for strings in LCursos:    
       cod_carrera=strings[0]
       cod_curso=strings[1]
       nombre = strings[2]
-      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
-                "Codigo de curso: "+cod_curso+"\n"+
-                "Nombre: "+nombre+"\n")
+##      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
+##                "Codigo de curso: "+cod_curso+"\n"+
+##                "Nombre: "+nombre+"\n")
+      reporte=cod_carrera+","+cod_curso+","+nombre
       #Reportes.write("Profesores")  
       Reportes.write(reporte)
-def reportGrupos():
-  for strings in LGrupos:   
+#cursos dentro de una carrera:
+def reportCursosporCarrera(Reportes,carrid):
+  Reportes.write("CodCarrera,CodCurso,Nombre\n")
+  for strings in LCursos:    
       cod_carrera=strings[0]
       cod_curso=strings[1]
-      cod_grupo=strings[2]
-      cod_prof=strings[3]
-      cupototal = strings[4]
-      cupomatriculado=strings[5]
-      cupocongelado= strings[6]
-      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
-                "Codigo de curso: "+cod_curso+"\n"+
-                "Codigo de grupo: "+cod_grupo+"\n"+
-                "Codigo de profesor: "+cod_prof+"\n"+
-                "Cupo Total: "+cupototal+"\n"+
-                "Cupo Matriculado: "+cupomatriculado+"\n"+
-                "Cupo Congelado: "+cupocongelado+"\n")
+      nombre = strings[2]
+      if cod_carrera==carrid:
+##      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
+##                "Codigo de curso: "+cod_curso+"\n"+
+##                "Nombre: "+nombre+"\n")
+        reporte=cod_carrera+","+cod_curso+","+nombre
       #Reportes.write("Profesores")  
-      Reportes.write(reporte)
-def reportEstuCurso():
+        Reportes.write(reporte)
+##def reportGrupos(Reportes):
+##  Reportes.write("CodCarrera,CodCurso,CodGrupo,CodProfesor, CupoTotal, CupoMatriculado, CuposCongelados\n")
+##  for strings in LGrupos:   
+##      cod_carrera=strings[0]
+##      cod_curso=strings[1]
+##      cod_grupo=strings[2]
+##      cod_prof=strings[3]
+##      cupototal = strings[4]
+##      cupomatriculado=strings[5]
+##      cupocongelado= strings[6]
+####      reporte = ("Codigo de carrera: "+cod_carrera+"\n"+
+####                "Codigo de curso: "+cod_curso+"\n"+
+####                "Codigo de grupo: "+cod_grupo+"\n"+
+####                "Codigo de profesor: "+cod_prof+"\n"+
+####                "Cupo Total: "+cupototal+"\n"+
+####                "Cupo Matriculado: "+cupomatriculado+"\n"+
+####                "Cupo Congelado: "+cupocongelado+"\n")
+##      reporte= cod_carrera+","+cod_curso+","+cod_grupo+","+cod_prof+","+cupototal+","+cupomatriculado+","+cupocongelado
+##      #Reportes.write("Profesores")  
+##      Reportes.write(reporte)
+def reportEstuCurso(Reportes):
+  Reportes.write("Ident-Estudiante,CodCurso,CodGrupo,Estado\n")
   for strings in LEstuCurso:    
       ident_estu=strings[0]
       cod_curso=strings[1]
       cod_grupo = strings[2]
       category = ""
-      if strings[3]=="1" :
+      if strings[3]=="1\n" :
         category= "Matriculado"
-      if strings[3]=="2" :
+      if strings[3]=="2\n" :
         category= "Desmatriculado"
-      if strings[3]=="3" :
+      if strings[3]=="3\n" :
         category= "Congelado"
-      reporte = ("Identificacion de estudiante: "+ident_estu+"\n"+
-                "Codigo de curso: "+cod_curso+"\n"+
-                "Codigo de grupo: "+cod_grupo+"\n"+
-                "Estado: "+category+"\n")
-      #Reportes.write("Profesores")  
+##      reporte = ("Identificacion de estudiante: "+ident_estu+"\n"+
+##                "Codigo de curso: "+cod_curso+"\n"+
+##                "Codigo de grupo: "+cod_grupo+"\n"+
+##                "Estado: "+category+"\n")
+      reporte=ident_estu+","+cod_curso+","+cod_grupo+","+category
+      #Reportes.write("Profesores")
+      #print(type(strings[3]))
       Reportes.write(reporte)
-def reportEstudiantes():
+def reportEstudiantes(Reportes):
+  Reportes.write("Ident-Estudiante,CodCarrera,Nombre,Direccion,Telefono\n")
   for strings in LEstudiantes:    
       ident_estu=strings[0]
       cod_carrera=strings[1]
       nombre = strings[2]
-      direccion=[3]
-      telefono=[4]
-      reporte = ("Identificacion de estudiante: "+ident_estu+"\n"+
-                "Codigo de carrera: "+cod_carrera+"\n"+
-                "Nombre: "+nombre+"\n"+
-                "Direccion: "+direccion+"\n"+
-                "telefono: "+telefono+"\n")
+      direccion=strings[3]
+      telefono=strings[4]
+##      reporte = ("Identificacion de estudiante: "+ident_estu+"\n"+
+##                "Codigo de carrera: "+cod_carrera+"\n"+
+##                "Nombre: "+nombre+"\n"+
+##                "Direccion: "+direccion+"\n"+
+##                "telefono: "+telefono+"\n")
+      reporte= ident_estu+","+cod_carrera+","+nombre+","+direccion+","+telefono
       #Reportes.write("Profesores")  
       Reportes.write(reporte)
-def reportProfesores():
+def reportProfesores(Reportes):
+  Reportes.write("CodProfesor, CodCarrera,Categoría,Nombre\n")
   for strings in LProfesores:
       
       cod_prof=strings[0]
       cod_carrera=strings[1]
       category = "Servicio" if strings[2]=="1" else "Carrera"
       nombre = strings[3]
-      reporte = ("Codigo de profesor: "+cod_prof+"\n"+
-                "Codigo de carrera: "+cod_carrera+"\n"+
-                "Categoria: "+category+"\n"+
-                "Nombre: "+nombre+"\n")
+##      reporte = ("Codigo de profesor: "+cod_prof+"\n"+
+##                "Codigo de carrera: "+cod_carrera+"\n"+
+##                "Categoria: "+category+"\n"+
+##                "Nombre: "+nombre+"\n")
+      reporte= cod_prof+","+cod_carrera+","+category+","+nombre
       #Reportes.write("Profesores")  
       Reportes.write(reporte)
+def reportProfesoresServ(Reportes):
+  Reportes.write("CodProfesor, CodCarrera,Categoría,Nombre\n")
+  for strings in LProfesores:      
+      cod_prof=strings[0]
+      cod_carrera=strings[1]
+      category = "Servicio" if strings[2]=="1" else "Carrera"
+      if category== "Servicio":       
+        nombre = strings[3]
+##        reporte = ("Codigo de profesor: "+cod_prof+"\n"+
+##                  "Codigo de carrera: "+cod_carrera+"\n"+
+##                  "Categoria: "+category+"\n"+
+##                  "Nombre: "+nombre+"\n")
+        reporte= cod_prof+","+cod_carrera+","+category+","+nombre
+        #Reportes.write("Profesores")  
+        Reportes.write(reporte)
+def reportProfesoresCarrera(Reportes,carrid):
+  Reportes.write("CodProfesor, CodCarrera,Categoría,Nombre\n")
+  for strings in LProfesores:
+      
+      cod_prof=strings[0]
+      cod_carrera=strings[1]
+      category = "Servicio" if strings[2]=="1" else "Carrera"
+      if cod_carrera== carrid:
+        nombre = strings[3]
+##        reporte = ("Codigo de profesor: "+cod_prof+"\n"+
+##                  "Codigo de carrera: "+cod_carrera+"\n"+
+##                  "Categoria: "+category+"\n"+
+##                  "Nombre: "+nombre+"\n")
+        reporte= cod_prof+","+cod_carrera+","+category+","+nombre
+        #Reportes.write("Profesores")  
+        Reportes.write(reporte)
   # print(LProfesores)
 ###################### BOOTS  ############################     
 loadcarreras()
 loadcursos()
+loadgrupos()
 loadestucurso()
 loadestudiantes()
 loadprofesores()
 
-reportCarreras()
-reportCursos()
-reportGrupos()
-reportProfesores()
+##reportCarreras()
+##reportCursos()
+##reportGrupos()
+##reportEstuCurso()
+##reportEstudiantes()
+##reportProfesores()
 ############ MAIIIN   ##########################################
 def main():
   while True:
@@ -176,13 +237,87 @@ def main():
         loopReportes()
       if respuesta == "s" or respuesta == "S":
         break
-def loopMatricular():
+##def loopMatricular():
+##
+##def loopDesmatricular():
+##
+##def loopCongelar():
 
-def loopDesmatricular():
+def loopReportes():
+  while True:
+      print("Que reporte desea hacer:")
+      print("1) Carreras")
+      print("2) Cursos de una carrera")
+      print("3) Profesores")    
+      print("4) Profesores de un curso")
+      print("5) Estudiantes de un grupo")
+      print("6) Estudiantes de una carrera")
+      print("7) Profesores de Servicio")
+      print("8) Profesores de una carrera")
+      print("9) Cantidad de personas atendidas por mostrador")
+      print("S) Salir")
+      respuesta=input(": ")
+      filename=input("Digite el nombre del archivo--> ")
+      filename="Reports/"+filename
+      
+      if respuesta == "1":
+        Reportes = open(filename+".csv",'a')
+        reportCarreras(Reportes)
+        Reportes.close()
+        print("Las carreras han sigo agregadas al archivo "+filename)
+        print("")
+      if respuesta == "2":
+        codcar=input("Digite el codigo de la carrera: ")
+        Reportes = open(filename+".csv",'a')
+        reportCursosporCarrera(Reportes,codcar)
+        Reportes.close()
+        print("Los cursos de esa carrera han sigo agregados al archivo "+filename)
+        print("")
+        
+      if respuesta == "3":
+        Reportes = open(filename+".csv",'a')
+        reportProfesores(Reportes)
+        Reportes.close()
+        print("Los profesores han sido agregados al archivo REPORTE.txt")
+        print("")
+      if respuesta == "4":
+        res=input("Digite el curso-->")
 
-def loopCongelar():
+      if respuesta == "5":
+        res=input("Digite el grupo-->")
 
-def loopREportes():
+      if respuesta == "6":
+        res=input("Digite la carrera-->")
+
+      if respuesta == "7":
+        break
+        
+      if respuesta == "8":
+        codcarr=input("Digite el codigo de la carrera: ")
+        Reportes = open(filename+".csv",'a')
+        reportProfesoresCarrera(Reportes,codcarr)
+        Reportes.close()
+        print("Las carreras han sigo agregadas al archivo "+filename)
+        print("")
+
+##      if respuesta == "9":
+##        Reportes = open(filename+".csv",'a')
+##        reportGrupos(Reportes)
+##        Reportes.close()
+##        print("Las carreras han sigo agregadas al archivo "+filename)
+##        print("")  
+
+
+      if respuesta == "s" or respuesta == "S":
+          break
+
+
+
+
+
+main()
+
+
 
 OpenCarreras.close()
 OpenCursos.close()
